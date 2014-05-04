43561788b730af3662f672c050e1517f5d47fd11
noboru-i <ishikura.noboru@gmail.com>
2014-05-05 01:10:08 +0900

    deviseのテンプレートを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end
