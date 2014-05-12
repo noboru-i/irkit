b7252e1e080622e2322b3cf136b5ea610a55d33d
noboru-i <ishikura.noboru@gmail.com>
2014-05-13 01:16:29 +0900

    unicorn.rbを追加

---
+++ config/unicorn.rb
@@ -0,0 +1,22 @@
+@dir = "/srv/web/current"
+application = 'web'
+
+#pidを保存するファイル
+listen "/srv/web/shared/pids/unicorn_#{application}.sock"
+pid "/srv/web/shared/pids/unicorn_#{application}.pid"
+
+worker_processes 2 # CPUのコア数に揃える
+working_directory @dir
+preload_app true
+
+timeout 300
+listen 80
+
+# ログ出力
+if ENV['RAILS_ENV'] == 'development'
+  stderr_path "/dev/tty"
+  stdout_path "/dev/tty"
+else
+  stderr_path "#{@dir}/log/unicorn.stderr.log"
+  stdout_path "#{@dir}/log/unicorn.stdout.log"
+end
