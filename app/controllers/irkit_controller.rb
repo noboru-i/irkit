37306e2916aadcf15b87099b6d0e71222a579b88
noboru-i <ishikura.noboru@gmail.com>
2014-06-14 18:29:49 +0900

    IRKitのIPアドレス変更

--- app/controllers/irkit_controller.rb
+++ app/controllers/irkit_controller.rb
@@ -1,13 +1,13 @@
 class IrkitController < ApplicationController
   def get
-    irkit = IRKit::Device.new(address: '192.168.81.12')
+    irkit = IRKit::Device.new(address: '192.168.81.2')
     ir_data = irkit.get_messages
     render json: {json: ir_data.to_json}
   end
 
   def post
     command = Command.find(params[:command_id])
-    irkit = IRKit::Device.new(address: '192.168.81.12')
+    irkit = IRKit::Device.new(address: '192.168.81.2')
     irkit.post_messages(command.json)
     render json: {}
   end
